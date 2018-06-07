915df3b916e74c5e4c1a5d617530764088cc7ed6
Santhosh Gugan <santhoshg1990@gamil.com>
2018-06-07 21:55:08 +0530

    onboard flat component
    #sprint1

--- src/app/core/onboard/onboard.module.ts
+++ src/app/core/onboard/onboard.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SocialLoginComponent } from './social-login/social-login.component';
+import { OnboardComponent } from './onboard.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [SocialLoginComponent]
+  declarations: [SocialLoginComponent, OnboardComponent]
 })
 export class OnboardModule { }
