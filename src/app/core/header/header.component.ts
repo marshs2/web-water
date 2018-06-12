0763e667852bc86137a28b3932738a59b7f011b3
Santhosh Gugan <santhoshg1990@gmail.com>
2018-06-12 20:19:50 +0530

    loginsignup buttion routing
    #sprint1

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  @Output() loginSignupClick = new EventEmitter<boolean>();
   constructor() { }
 
   ngOnInit() {
+  }
+
+  loginSignUpClicked(){
+    this.loginSignupClick.emit(true);
   }
 
 }
